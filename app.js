a8f8265bf45b07daccb25dfc7d66642685b32ee5
SantaGosu <taz.c.munoz@gmail.com>
2022-07-22 23:08:28 +0000

    added inital arrays and pushed them together into one

---
+++ app.js
@@ -0,0 +1,8 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let mixedArray = [];
+
+mixedArray.push(...pronoun, ...adj, ...noun);
+
+console.log(mixedArray);
