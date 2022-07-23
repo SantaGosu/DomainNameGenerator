ab1bdbbb6fd7b773370de32ba5a789c04b99b267
SantaGosu <taz.c.munoz@gmail.com>
2022-07-23 03:34:20 +0000

    working on forEach function

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let mixArr = pronoun.concat(adj, noun);
 
-for(i = 0; i<pronoun.length; i++){
-    for(k =0; k<adj.length; k++){
-        for(j =0; j<noun.length; j++){ 
-        console.log(pronoun[i] + adj[k] + noun[j] + ".com");
-        }
-    }
-}
+pronoun.forEach((word1, index) => {
+    const word2 = adj[index];
+    const word3 = noun[index];
+    console.log(word1 + word2 + word3 + '.com');
+});
 
-// pronoun.forEach((index) => {
-//     console.log(index + '.com');
-// });
 
-//try for each loop
-//Add several types of extensions: .com, .net, .us, .io, etc.
+// for(i = 0; i<pronoun.length; i++){
+//     for(k =0; k<adj.length; k++){
+//         for(j =0; j<noun.length; j++){ 
+//         console.log(pronoun[i] + adj[k] + noun[j] + ".com");
+//         }
+//     }
+// }
+
+
+// // //try for each loop
+// // //Add several types of extensions: .com, .net, .us, .io, etc.
+
+
+
+
+
 
